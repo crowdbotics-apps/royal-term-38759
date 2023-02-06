e4035778569280a808220c80dfe2c18122e7a3e2
Crowdbotics <team@crowdbotics.com>
2023-02-06 22:24:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-term-38759",
+  url: "https://royal-term-38759.botics.co",
+  api: "https://royal-term-38759.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
